5937cbb99efa81476309a7447e3d9a422743d63c
gongvic000 <53986086+gongvic000@users.noreply.github.com>
2022-01-02 00:11:02 -0600

    updates

--- send.py
+++ send.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-import messages, recipients, smtplib, ssl, states, sys, time
+import messages, senators, smtplib, ssl, states, sys, time
 from getpass import getpass
 from email.message import EmailMessage
 
-def print_barrier():
+def print_success():
     print("======================================================")
 
 def prompt_login():
-    print_barrier()
+    print_success()
     name = ""
-    # Validate name is not blank
+    # make sure name not blank
     while True:
         if not name:
             name = input("Type your name and press enter: ")
         else:
             break
 
-    # Maybe add some validation for "@gmail" suffix
-    email = input("Type your email and press enter: ")
+    email = input("Type your gmail and press enter: ")
     password = getpass("Type your password and press enter: ")
-    print_barrier()
+    print_success()
     return name, email, password
 
 
 
 def prompt_email():
-    print_barrier()
+    print_success()
     print("\nWhat would you like the subject (title) of your email to be?\n")
     subject = input("Type here and press enter (if blank, a random one will be generated): ")
-    print_barrier()
-    print("\nMailbot can write unique emails addressed personally to each lawmaker.")
+    print_success()
+    print("\nThis program creates unique and personalized emails to Senators.")
     print("However, if you would like to write your own message, please save it in a .txt file. The easiest way to do this is to just write your message in example.txt.\n")
     while True:
         response = input("Would you like mailbot to write emails for you? (y/n): ")
@@ -55,59 +54,31 @@ def prompt_recipients():
 
     # Choose a state
     while True:
-        print_barrier()
-        print("Which state officials do you want to send emails to?")
-        if cart: print(f'Cities chosen: {cart}\n')
-        state_options = { v:k for v,k in enumerate(recipients.get_states()) }
+        print_success()
+        print("Which state senators do you want to send emails to?")
+        if cart: print(f'States chosen: {cart}\n')
+        state_options = { v:k for v,k in enumerate(senators.get_states()) }
         for idx, opt in state_options.items():
             print(idx, "->", opt)
         print("Enter blank (nothing) when done.")
         
         state_idx = input("\nType the number corresponding to the state here: ")
-        print_barrier()
+        print_success()
         # Blank -> Done
         if not state_idx:
             break
         # 0 -> All States
         elif int(state_idx) == 0:
-            recv.update(recipients.get_all())
+            recv.update(senators.get_all())
             break
         # (1 to N) -> Individual States
         elif int(state_idx) in state_options.keys():
             state = state_options[int(state_idx)]
             subcart = set()
-
-            # Choose a city
-            while True:
-                city_options = { v:k for v,k in enumerate(recipients.get_cities(state)) }
-                print_barrier()
-                print("Which city officials do you want to send emails to?")
-                if subcart: print(f'Cities chosen: {subcart}\n')
-                for idx, opt in city_options.items():
-                    print(idx, "->", opt)
-                print("Enter blank (nothing) when done.")
-                city_idx = input("\nType the number corresponding to the city here: ")
-        
-                if not city_idx:
-                    break
-                elif int(city_idx) == 0:
-                    subcart.update(recipients.get_cities(state))
-                    subcart.remove('Select All')
-                    recv.update(recipients.get_state(state))
-                    break
-                elif int(city_idx) in city_options.keys():
-                    subcart.add(city_options[int(city_idx)])
-                    recv.update(recipients.get_city(state, city_options[int(city_idx)]))
-                else:
-                    print("Invalid index")
-            # Add (city, state) to cart)
-            for city in subcart:
-                cart.add("%s, %s" % (city, states.abbreviate(state)))
-            print_barrier()
         
         else:
             print("Invalid index")
-    print_barrier()
+    print_success()
 
     if not recv:
         sys.exit("ABORT: no recipients selected.")
@@ -157,6 +128,6 @@ while True:
         print("Unexpected error... trying again in 10 seconds.")
         time.sleep(10)
 
-print_barrier()
+print_success()
 print(f'\nSuccessfully sent {send} emails!\n')
-print_barrier()
+print_success()
